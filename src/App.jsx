2b86d515bab150a315e04f0f4598555e2acd55e2
Soufianetalibi <Soufiane.talibi@gmail.com>
2025-07-16 16:39:43 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <h1 className="text-4xl font-bold text-blue-600">Hello from GitHub Pages + Tailwind!</h1>
+    </div>
+  )
+}
+export default App
